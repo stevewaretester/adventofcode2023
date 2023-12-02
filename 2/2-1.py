142bef2ab379996aac14ae7467c7dfd79aa4a1cc
stevewaretester <steveharland@googlemail.com>
2023-12-02 12:21:35 +0000

    2-1 complete (stupid trailing newlines)

---
+++ 2/2-1.py
@@ -0,0 +1,71 @@
+#https://adventofcode.com/2023/day/2
+import os
+import sys
+
+
+def txtToLineArray(txt):
+    file = open(txt, "r")
+    lines = file.readlines()
+    file.close()
+    return lines
+
+
+input = os.path.join(sys.path[0],"input.txt")
+
+lines = txtToLineArray(input)
+
+
+#exampleLines
+#lines = ["Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green","Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue","Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red","Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red","Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"]
+
+maxRed = 12
+maxGreen = 13
+maxBlue = 14
+
+totalGood = 0
+sumTotal = 0
+for line in lines:#split by lines
+    print(line)
+
+    impossGame = False#a boolean we can set to "true" when the game is impossible.
+
+    gameid = ""
+    gameResultsplit = line.split(":")
+    gameheader = gameResultsplit[0]
+    gameid = gameheader.split(" ")[1]#get the gameheader from the first half of the split, by splitting that half on a space and taking the second half(quarter?)
+    sumTotal+=int(gameid)
+    
+    #individualScores = gameResultsplit[1].replace(";",",").split(",")
+
+    rounds = gameResultsplit[1].split(";")#split by semicolon into "rounds" eg. what cubes all appeared on the table at once.
+
+    for round in rounds:#going round-by-round...
+        individualscore = round.split(",")#split these into individual pairins of cubes and scores
+        sumRed = 0#we need to know all the reds.
+        sumGreen = 0#all the greens
+        sumBlue = 0#all the blues
+        for colourResult in individualscore:#each allocation of cubes.
+            #print(colourResult)
+            lstrippedCResult = colourResult.lstrip(' ').split(" ")#remove the leading space and split into two halves.
+            colour = lstrippedCResult[1].rstrip('\n')#eg. blue, red, green (also remove the newline if there are any)
+            result = int(lstrippedCResult[0])#eg. the numerical value
+            
+            if colour=="red":
+                sumRed += result
+            elif colour=="green":
+                sumGreen += result
+            elif colour=="blue":
+                sumBlue += result
+
+        if (sumRed>maxRed) or (sumGreen>maxGreen) or (sumBlue>maxBlue): #if any colour is greater than its max-allowed number...
+            impossGame = True #it was a bad game
+            print(round+" was a bad round")
+            break#if one round fails the entire game is impossible.
+    
+    if not impossGame: # if it was a possible game
+        totalGood+=int(gameid)#take its ID and add it to the stack
+        print("Good Game"+gameid+", totalgood: "+str(totalGood))
+    else:
+        print("Bad Game: "+gameid)
+
+print("total sum of possible games: "+str(totalGood))
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # adventofcode2023
-my advent of code work.
+my advent of code work - if interested feel free to join a leaderboard of mine to see how you did:
+
+3278293-29c0c75c
